6cb18430321ab2b3ad11099b2598647aaaf63df5
lucero garcia <lucerogarcia@macbook-pro-de-lucero.local>
2016-01-08 22:40:48 -0600

    juego de memoria

--- app1/ViewController.swift
+++ app1/ViewController.swift
@@ -5,22 +5,23 @@
 //  Created by lucero garcia on 07/01/16.
 //  Copyright © 2016 lucerogarcia. All rights reserved.
 //
+//: Playground - noun: a place where people can play
+//juego de memoria
 
 import UIKit
 
 class ViewController: UIViewController {
     //modificando....
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
